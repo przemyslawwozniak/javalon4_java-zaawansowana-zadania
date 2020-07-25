10cdab4832d0714fc13dc8e9bffa9b030509d9d0
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-07-25 12:39:56 +0200

    Ex 8 - init point

---
+++ src/main/java/pl/sda/javalon4/collections/TextToLettersIndexes.java
@@ -0,0 +1,25 @@
+package pl.sda.javalon4.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+1. Zdefiniowac napis do analizy:
+String line = "Ala ma kota i Jarek tez ma kota."
+
+2. Map<Character, Integer[]>
+a = [2, 5, 10...]
+
+3. String -> char[] ---> String.toCharArray()
+
+4. W petli for lecimy po kazdym znaku
+
+5. Dla kazdego znaku odczytujemy jego pozycje (index) w ciagu String i dodajemy do odpowiedniego klucza mapy - aktualizacja mapy poprzez modyfikacje (dodanie kolejnego indeksu) wartosci przypisanej do klucza (tablicy)
+ */
+public class TextToLettersIndexes {
+
+    public static void main(String[] args) {
+        Map<Character, Integer[]> lettersMap = new HashMap<>();
+    }
+
+}
